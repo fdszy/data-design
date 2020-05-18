b77ddbebafc9fb13b7a19daaa96aab150c7b63c2
fdszy <44485783+fdszy@users.noreply.github.com>
2020-05-18 11:28:33 +0800

    购买机票
    image文件夹用于存放可能需要的图片票

---
+++ buy_ticket.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->
+  <title>航空机票管理系统</title>
+  <!-- Bootstrap -->
+  <link href="./static/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Css -->
+  <link rel="stylesheet" href="./static/css/offcanvas.css">
+  <link rel="stylesheet" href="./static/css/bootstrap-datetimepicker.min.css">
+  <style> body {
+            font-family: "Microsoft YaHei UI", "Droid Sans Mono", serif;
+        }
+    </style>
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+    <script src="https://cdn.bootcss.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+  <div id="app">
+    <nav class="navbar navbar-fixed-top navbar-inverse">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">航空机票管理系统</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+             <li><a href="./indexx.html">首页</a></li>
+            <li><a href="./user.html">帐号管理</a></li>
+            <li><a href="./ticket.html">账户信息</a></li>
+              <li class="active"><a href="javascript:void(0)">购票</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="./login.html">欢迎,<?php session_start();echo $_SESSION['user'];?></a></li>
+          </ul>
+        </div><!-- /.nav-collapse -->
+      </div><!-- /.container -->
+    </nav><!-- /.navbar -->
+    <h5>账户余额：0 </h5>
+      <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar">
+    <div class="panel panel-default">
+      <style>
+        .form-group {
+          height: 200px;
+          width: 300px;
+      </style>
+      <div class="panel-heading"> 机票搜索 </div>
+      <div class="panel-body">
+        <form role="form">
+          <div class="form-group">
+            <label for="name">出发地</label>
+            <select class="form-control">
+              <option>北京</option>
+              <option>上海</option>
+            </select>
+            <label for="name">到达地</label>
+            <select class="form-control">
+              <option>北京</option>
+              <option>上海</option>
+            </select>
+            <label for="name">仓位</label>
+            <select class="form-control">
+              <option>头等舱</option>
+              <option>经济舱</option>
+            </select>
+              <label for="name"></label>
+              <div class="form-group">
+                <button type="submit" class="btn btn-primary form-control">查询</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+      </div>
+    <p><img src="image/ticket_plane.jpg"class = "img-round" width="900"height="300"</p>
+        <div class="panel panel-default">
+      <div class="panel-heading"> 查询结果 </div>
+      <div class="panel-body">
+          <div class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="table-responsive">
+            <table class="table table-bordered ">
+              <thead class="thead-dark">
+                <tr>
+                  <th>出发城市</th>
+                  <th>出发机场</th>
+                  <th>到达城市</th>
+                  <th>到达机场</th>
+                  <th>出发时间</th>
+                  <th>到达时间</th>
+                  <th>机票价格</th>
+                  <th>订票</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>null</th>
+                  <td>null</td>
+                  <td>null</td>
+                  <td>null</td>
+                  <td>null</td>
+                  <td>null</td>
+                  <td>null</td>
+                  <td><a class="btn btn-primary" href="#"><i class="fa fa-download fa-fw"></i>购买机票</a></td>
+                </tr>
+                <tr></tr>
+                <tr></tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+      </div>
+      </di</div>
+  </div>
+
+</body>
+
+</html>
