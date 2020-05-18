4a94f4071e231ff78a59acfcb0f52ff8c303ceef
Dead-Soul <57261488+Dead-Soul@users.noreply.github.com>
2020-05-18 14:30:53 +0800

    登录和注册优化

--- login.php
+++ login.php
@@ -35,20 +35,20 @@ if ($stmt = $mysqli->prepare($query))
     {
         // 逐条从MySQL服务取数据
 	    $a = 1;
-	    if(md5($pwd,$salt) == $password){
+	    if(md5($pwd+$salt,FALSE) == $password){
 		    echo "<script>alert('登陆成功！');</script>";
 		    $url = "login.html";
 		    $_SESSION['user'] = $name;
-		    header("Refresh:1;url=index.php");
+		    header("Refresh:0.5;url=index.php");
 	    }
 	    else{
 		    echo "<script>alert('用户名或密码错误');</script>";
-		    header("Refresh:1;url=login.html");
+		    header("Refresh:0.5;url=login.html");
 	    }
     }
   if($a == 0){ // no this user
 		echo "<script>alert('用户名或密码错误');</script>";
-		header("Refresh:1;url=login.html");
+		header("Refresh:0.5;url=login.html");
     }
 }
 
--- sign-up.php
+++ sign-up.php
@@ -6,29 +6,31 @@ $pwd = $_POST['password'];
 $confirm = $_POST['confirm'];
 $id = $_POST['id'];
 
-if($comfirm != $pwd){ // no this user
-  echo "<script>alert('两次输入密码不一致');</script>";
-  header("Refresh:1;url=sign-up.html");
-}
+if($comfirm == $pwd){
 
-$mysqli = new mysqli('47.101.211.158','mxy','123456','ticket_system');
 
-$salt = substr(str_shuffle("QWERTYUIOPASDFGHJKLZXCVBNM1234567890qwertyuiopasdfghjklzxcvbnm"),mt_rand(0,strlen($strs)-11),10);
-$pwd = md5($pwd,$salt);
+  $mysqli = new mysqli('47.101.211.158','mxy','123456','ticket_system');
 
-$query = "INSERT customer (id,name,pwd_hash,salt,balance) VALUES (?,?,?,?,0.00)";
-if ($stmt = $mysqli->prepare($query))
-{
+  $strs = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890qwertyuiopasdfghjklzxcvbnm";
+  $salt = substr(str_shuffle($strs),mt_rand(0,strlen($strs)-11),10);
+  $pwd = md5($pwd+$salt,FALSE);
+
+  $query = "INSERT customer (id,name,pwd_hash,salt,balance) VALUES (?,?,?,?,0.00)";
+  if ($stmt = $mysqli->prepare($query)){
     $stmt->bind_param('ssss', $id,$name,$pwd,$salt);
     if($stmt->execute()){
 		  echo "<script>alert('注册成功，正在跳转到登陆界面...');</script>";
-		  header("Refresh:1;url=login.html");
+		  header("Refresh:0.5;url=login.html");
     }
 	  else{
 		  echo "<script>alert('注册失败，请重试');</script>";
-		  header("Refresh:1;url=sign-in.html");
+		  header("Refresh:0.5;url=sign-in.html");
 	  }
+  }
 }
-
+else{
+  echo "<script>alert('两次输入密码不一致');</script>";
+  header("Refresh:0.5;url=sign-up.html");
+}
 
 ?>
