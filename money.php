e702587a9407ba9a48e459287bcdd6a4131107e1
fdszy <44485783+fdszy@users.noreply.github.com>
2020-06-07 00:22:08 +0800

    一定要一人一次哦

--- money.php
+++ money.php
@@ -10,10 +10,10 @@ $money = $_POST['money'];
 
 $mysqli = new mysqli('47.101.211.158','mxy','123456','ticket_system');
 
-$query = "UPDATE customer SET balance = ? WHERE id = ?";
+$query = "UPDATE customer SET balance = balance + ? WHERE id = ?";
 
 if ($stmt = $mysqli->prepare($query)){
-    $stmt->bind_param('ds', ((double)$money+(double)$_SESSION['user']['balance']), $_SESSION['user']['id']);
+    $stmt->bind_param('ds', ((double)$money, $_SESSION['user']['id']);
     if($stmt->execute()){
           echo "<script>alert('充值成功，正在跳转购票页面...');</script>";
           echo "<script language='javascript' type='text/javascript'>window.location.href='./buy_ticket.php'</script>";
