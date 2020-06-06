251fa892d19f7461f980aed2c085a92f57d0472b
fdszy <44485783+fdszy@users.noreply.github.com>
2020-06-07 00:01:46 +0800

    额额

--- money.php
+++ money.php
@@ -13,7 +13,7 @@ $mysqli = new mysqli('47.101.211.158','mxy','123456','ticket_system');
 $query = "update customer set balance = ? where id = ?";
 
 if ($stmt = $mysqli->prepare($query)){
-    $stmt->bind_param('s', $_SESSION['user']['id']);
+    $stmt->bind_param('ss', $money,$_SESSION['user']['id']);
     if($stmt->execute()){
           echo "<script>alert('充值成功，正在跳转购票页面...');</script>";
           echo "<script language='javascript' type='text/javascript'>window.location.href='./buy_ticket.php'</script>";
