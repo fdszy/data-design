65baf8a3c97ccba731a4bd5da7cc2cca586c1443
fdszy <44485783+fdszy@users.noreply.github.com>
2020-06-08 23:01:53 +0800

    bug_commit

---
+++ pro_bug.php
@@ -0,0 +1,23 @@
+<?php
+
+include 'functions.php';
+session_start();
+if(!isset($_SESSION['user'])){
+    exit;
+}
+
+$bug = $_POST['bug'];
+$mysqli = new mysqli('47.101.211.158','mxy','123456','ticket_system');
+
+$query = "insert into buginfo (submitter,description,isfixed) values('?','?','N')";
+
+if ($stmt = $mysqli->prepare($query)){
+    $stmt->bind_param('ss',$_SESSION['user']['id'],$bug);
+    if($stmt->execute()){
+          echo "<script>alert('bug信息提交成功，感谢...');</script>";
+          echo "<script language='javascript' type='text/javascript'>window.location.href='./index.php'</script>";
+
+          exit;
+        }
+
+}
