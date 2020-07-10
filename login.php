a5acfbbd7014b48f894e98dd19a8471f9810b2f7
Dead-Soul <57261488+Dead-Soul@users.noreply.github.com>
2020-07-10 13:30:11 +0800

    Update login.php

--- login.php
+++ login.php
@@ -8,8 +8,7 @@ $pwd = $_POST['password'];
 $mysqli = new mysqli('47.101.211.158','mxy','123456','ticket_system');
 
 $query = "SELECT id,name,balance,pwd_hash,salt,credit FROM customer WHERE name = ?";
-if ($stmt = $mysqli->prepare($query))
-{
+if ($stmt = $mysqli->prepare($query)){
     $stmt->bind_param('s', $user);
     $stmt->execute();
     $stmt->bind_result($id, $name, $balance, $password, $salt, $credit);
@@ -19,10 +18,8 @@ if ($stmt = $mysqli->prepare($query))
 	  echo "<script>alert('用户名或密码错误');</script>";
       echo "<script language='javascript' type='text/javascript'>window.location.href='./login.html'</script>";
     }
-    while ($stmt->fetch())
-    {
-	    if(md5($pwd+$salt,FALSE) == $password){
-		    //echo "<script>alert('登陆成功！');</script>";
+    while ($stmt->fetch()){
+	    if(md5($pwd.$salt,FALSE) == $password){
             $_SESSION['user'] = array('name'=>$name,'id'=>$id,'balance'=>$balance,'credit'=>$credit);
             if($name === 'admin'){
                 echo "<script language='javascript' type='text/javascript'>window.location.href='./admin_user.php'</script>";
